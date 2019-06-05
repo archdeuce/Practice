073dcfa7885a3f05e7f336525b854d95f1d5346b
Oleg Reshetilo <archdeuce@gmail.com>
2019-06-05 11:54:25 +0300

    Task_1

---
+++ Task_1/Bicycle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Bicycle : Vehicle
+    {
+        public BicycleType Type { get; set; }
+
+        public BreakType Brake { get; set; }
+
+        public Bicycle(BicycleType bicycleType, BreakType breakType, int wheelCount, ConsoleColor color, int maxSpeed, int passengerCount)
+            : base(wheelCount, color, maxSpeed, passengerCount)
+        {
+            this.Type = bicycleType;
+
+            this.Brake = breakType;
+        }
+
+    }
+
+    enum BicycleType
+    {
+        RoadBike = 0,
+        MountainBike = 1,
+        BMX = 2
+    }
+
+    enum BreakType
+    {
+        HandBrakes = 0,
+        FootBrakes = 1
+    }
+}
