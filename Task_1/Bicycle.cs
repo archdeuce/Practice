e71d5d5c22899c98ee8d0e1a39aa07ed04cf74d8
Oleg Reshetilo <archdeuce@gmail.com>
2019-06-05 11:48:53 +0300

    Task_1

---
+++ Task_1/Bicycle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Bicycle : Vehicle
+    {
+        public BicycleType Type { get; set; }
+        public BreakType Brake { get; set; }
+
+        public Bicycle(BicycleType bicycleType, BreakType breakType, int wheelCount, ConsoleColor color, int maxSpeed, int passengerCount)
+            : base(wheelCount, color, maxSpeed, passengerCount)
+        {
+            Type = bicycleType;
+            Brake = breakType;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.ForegroundColor = tableColor;
+
+            Console.WriteLine(String.Format("{0, -10} {1, -5} {2, -8} {3, -14} {4, -11} {5, -9}", "WheelCount", "Color", "MaxSpeed", "PassengerCount", "BicycleType", "BreakType"));
+
+            Console.WriteLine(String.Format("{0, -10} {1, -5} {2, -8} {3, -14} {4, -11} {5, -9}", WheelCount, Color, MaxSpeed, PassengerCount, Type, Brake) + "\n");
+
+            Console.ResetColor();
+        }
+    }
+
+    public enum BicycleType
+    {
+        RoadBike,
+        MountainBike,
+        BMX
+    }
+
+    public enum BreakType
+    {
+        HandBrakes,
+        FootBrakes
+    }
+}
---
+++ Task_1/Vehicle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Vehicle
+    {
+        private int wheelCount = 1;
+        private int maxSpeed = 1;
+
+        public ConsoleColor Color { get; set; }
+
+        public int PassengerCount { get; set; }
+
+        public ConsoleColor tableColor = ConsoleColor.Yellow;
+
+        public int WheelCount
+        {
+            get
+            {
+                return wheelCount;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    wheelCount = value;
+                }
+            }
+        }
+
+        public int MaxSpeed
+        {
+            get
+            {
+                return maxSpeed;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    maxSpeed = value;
+                }
+            }
+        }
+
+        public Vehicle(int wheelCount, ConsoleColor color, int maxSpeed, int passengerCount)
+        {
+            WheelCount = wheelCount;
+            Color = color;
+            MaxSpeed = maxSpeed;
+            PassengerCount = passengerCount;
+        }
+
+        public void Move(int meters)
+        {
+            Console.ForegroundColor = Color;
+
+            Console.WriteLine($"{GetType().Name} has moved {meters} meters.");
+
+            Console.ResetColor();
+
+        }
+    }
+}
