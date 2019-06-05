26ba90bbfa820aa92cd9c7c4f61b87063d3e308f
Oleg Reshetilo <archdeuce@gmail.com>
2019-06-05 12:00:01 +0300

    Task_1

---
+++ Task_1/Arboreal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Arboreal : Mammal
+    {
+        public Arboreal(int weight, int age, string beast)
+            : base(weight, age, beast)
+        {
+        }
+    }
+}
---
+++ Task_1/Feeder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Feeder
+    {
+        Random rand = new Random();
+
+        public void Feed(Mammal[] mammals)
+        {
+            foreach (var item in mammals)
+            {
+                item.Eat(this.rand.Next(50));
+            }
+        }
+    }
+}
