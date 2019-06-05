45c6f6583d792de113d10b12ebc05cf5954fa7c9
Oleg Reshetilo <archdeuce@gmail.com>
2019-06-05 14:45:22 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = Singleton.GetInstance();
+
+            Console.Read();
+        }
+    }
+
+    class Singleton
+    {
+        private static Singleton instance;
+
+        public int someField;
+
+        public void DoSome()
+        {
+        }
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+
+            return instance;
+        }
+    }
+}
