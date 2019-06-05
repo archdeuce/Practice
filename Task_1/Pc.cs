699777e0039c69d6a3cd981f358b185de6f37f5a
Oleg Reshetilo <archdeuce@gmail.com>
2019-06-05 15:29:02 +0300

    Task_1

---
+++ Task_1/Pc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Pc : Device
+    {
+        public Pc(bool state)
+        {
+            this.State = state;
+        }
+    }
+}
