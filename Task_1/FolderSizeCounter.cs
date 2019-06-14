f24b5ebd8c55e655f0a57f066d99edd1d4573666
Oleg Reshetilo <archdeuce@gmail.com>
2019-06-14 12:48:40 +0300

    Task_1-3

---
+++ Task_1/FolderSizeCounter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class FolderSizeCounter
+    {
+        public void Execute()
+        {
+            string[] filesList = Directory.GetFiles(Config.DirPath);
+
+            int folderCount = 0;
+            int fileCount = 0;
+
+            decimal dirTotalSize = FolderCounter(Config.DirPath, ref folderCount, ref fileCount);
+
+            Console.WriteLine(new string('-', 120));
+            Console.WriteLine($"Folder \"{Config.DirPath}\" has size " + String.Format("{0:0.00}", (dirTotalSize / 1048576m)) + " MB.");
+            Console.WriteLine($"Folder \"{Config.DirPath}\" сontains {folderCount} folders.");
+            Console.WriteLine($"Folder \"{Config.DirPath}\" сontains {fileCount} files.");
+        }
+
+        public decimal FolderCounter(string path, ref int dirs, ref int files)
+        {
+            decimal folderSize = 0;
+
+            string[] filesList = Directory.GetFiles(path);
+
+            string[] dirList = Directory.GetDirectories(path);
+
+            dirs += dirList.Length;
+            files += filesList.Length;
+
+            foreach (var file in filesList)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+
+                folderSize += fileInfo.Length;
+            }
+
+            if (dirList.Length == 0)
+                return folderSize;
+
+            foreach (string dir in dirList)
+            {
+                var currentDirSize = FolderCounter(dir, ref dirs, ref files);
+
+                folderSize += currentDirSize;
+
+                Console.WriteLine($"\"{dir}\" Size: " + String.Format("{0:0.00}", (currentDirSize / 1048576m)) + " MB.");
+            }
+
+            return folderSize;
+        }
+    }
+}
---
+++ Task_2/FileManagement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public class FileManagement
+    {
+        public void WriteData()
+        {
+            Directory.CreateDirectory(Config.DirPath);
+
+            Console.WriteLine("Enter the text to write to the text file:");
+
+            string userInput = Console.ReadLine() + '\n';
+
+            File.AppendAllText(Config.FullPath, userInput);
+
+            Console.WriteLine("Information successfully recorded.");
+        }
+    }
+}
