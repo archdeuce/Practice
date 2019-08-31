d7f13181277b439f2f4f644ddcf03c1b2c494eda
Oleg Reshetilo <archdeuce@gmail.com>
2019-08-31 23:31:33 +0300

    This is already playable.

---
+++ Racing/Config.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racing
+{
+    public static class Config
+    {
+        //
+        // Window dimensions.
+        //
+        public const int ConsoleWidth = 45;
+        public const int ConsoleHeight = 30;
+
+        //
+        // The coordinates of the curb.
+        //
+        public const int RoadSideX1 = 0;
+        public const int RoadSideX2 = 22;
+
+        public const int RoadSideY1 = 0;
+        public const int RoadSideY2 = 30;
+
+        //
+        // The coordinates of the road.
+        //
+        public const int RoadX1 = RoadSideX1 + 1;
+        public const int RoadX2 = RoadSideX2 - 1;
+
+        public const int RoadY1 = RoadSideY1;
+        public const int RoadY2 = RoadSideY2;
+
+        //
+        // Dimensions of the machine.
+        //
+        public const int CarWidth = 6;
+        public const int CarHeight = 4;
+
+        //
+        // The coordinates of the player’s car.
+        //
+        public const int PlayerCarLeftPositionX = RoadSideX1 + 2;
+        public const int PlayerCarLeftPositionY1 = RoadSideY2 - CarHeight;
+        public const int PlayerCarLeftPositionY2 = RoadSideY2;
+
+        public const int PlayerCarRightPositionX = RoadSideX2 - 8;
+        public const int PlayerCarPositionY1 = RoadSideY2 - CarHeight;
+        public const int PlayerCarPositionY2 = RoadSideY2;
+
+        //
+        // The coordinates of the enemy machine.
+        //
+        public const int RivalCarLeftPositionX = RoadSideX1 + 2;
+        public const int RivalCarRightPositionX = RoadSideX2 - 8;
+
+        public const int RivalCarStartPositionY1 = RoadSideY1 - Config.CarHeight - 1;
+        public const int RivalCarStartPositionY2 = RoadSideY1 - 1;
+
+        //
+        // The coordinates of the info panel.
+        //
+        public const int InfoPanelPositionX1 = RoadSideX2 + 1;
+        public const int InfoPanelPositionX2 = InfoPanelPositionX1 + 14;
+
+        public const int InfoPanelPositionY1 = 0;
+        public const int InfoPanelPositionY2 = 30;
+
+        //
+        // The coordinates of the lives.
+        //
+        public const string LivesTitle = "L I V E S";
+
+        public const int LivesTitlePositionX = InfoPanelPositionX1 + 8;
+        public const int LivesTitlePositionY = InfoPanelPositionY1 + 2;
+
+        public const int LivesBarPositionX = InfoPanelPositionX1 + 8;
+        public const int LivesBarPositionY = InfoPanelPositionY1 + 4;
+
+        //
+        // The coordinates of the score.
+        //
+        public const string ScoreTitle = "S C O R E";
+        public const int MaxScore = 999999999;
+
+        public const int ScoreTitlePositionX = InfoPanelPositionX1 + 8;
+        public const int ScoreTitlePositionY = InfoPanelPositionY1 + 8;
+
+        public const int ScorePositionX = InfoPanelPositionX1 + 8;
+        public const int ScorePositionY = InfoPanelPositionY1 + 10;
+
+
+        //
+        // The coordinates of the level.
+        //
+        public const string SpeedTitle = "L E V E L";
+
+        public const int SpeedTitlePositionX = InfoPanelPositionX1 + 8;
+        public const int SpeedTitlePositionY = InfoPanelPositionY1 + 14;
+
+        public const int SpeedMeterPositionX = InfoPanelPositionX1 + 8;
+        public const int SpeedMeterPositionY = InfoPanelPositionY1 + 16;
+
+        //
+        // Game Over message position.
+        //
+        public const int GameOverX = 15;
+        public const int GameOverY = 12;
+        public const string GameOverText = "G A M E  O V E R";
+        public const string GameOverTotalScoreText = "S C O R E: ";
+
+        //
+        // Pause message position.
+        //
+        public const string PаuseTextTitle1 = "Start / Pause:";
+        public const int PаuseTextTitle1X = InfoPanelPositionX1 + 6;
+        public const int PаuseTextTitle1Y = 26;
+
+        public const string PаuseTextTitle2 = "<Space Button>";
+        public const int PаuseTextTitle2X = InfoPanelPositionX1 + 6;
+        public const int PаuseTextTitle2Y = 28;
+
+        //
+        // The number of lives.
+        //
+        public const int LivesNumber = 3;
+
+        //
+        // The distance between the cars of the enemy.
+        //
+        public const int RivalCarOffset = 18;
+    }
+}
---
+++ Racing/PlayerCar.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Racing
+{
+    public class PlayerCar
+    {
+        public int СarLeftPositionX { get; private set; }
+
+        public int СarRightPositionX { get; private set; }
+
+        public int СarPositionY1 { get; private set; }
+
+        public int СarPositionY2 { get; private set; }
+
+        public int CurrentPositionX { get; private set; }
+
+        public int CarHeight { get; private set; }
+
+        public PlayerCar()
+        {
+            this.CurrentPositionX = -1;
+            this.СarLeftPositionX = Config.PlayerCarLeftPositionX;
+            this.СarRightPositionX = Config.PlayerCarRightPositionX;
+            this.СarPositionY1 = Config.PlayerCarPositionY1;
+            this.СarPositionY2 = Config.PlayerCarPositionY2;
+            this.CarHeight = Config.CarHeight;
+        }
+
+        public void MoveLeft()
+        {
+            if (this.CurrentPositionX != this.СarLeftPositionX)
+            {
+                this.CurrentPositionX = this.СarLeftPositionX;
+
+                this.ClearOldCarPosition(this.СarRightPositionX, this.СarPositionY1);
+                this.DrawCar(this.СarLeftPositionX, this.СarPositionY1);
+            }
+        }
+
+        public void MoveRight()
+        {
+            if (this.CurrentPositionX != this.СarRightPositionX)
+            {
+                this.CurrentPositionX = this.СarRightPositionX;
+
+                this.ClearOldCarPosition(this.СarLeftPositionX, this.СarPositionY1);
+                this.DrawCar(this.СarRightPositionX, this.СarPositionY1);
+            }
+        }
+
+        public void RandomStartPosition()
+        {
+            this.CurrentPositionX = -1;
+
+            int r = new Random().Next(0, 2);
+
+            if (r == 0)
+                this.MoveLeft();
+            else
+                this.MoveRight();
+        }
+
+        private void DrawCar(int x, int y)
+        {
+            lock (Game.consoleLocker)
+            {
+                Console.SetCursorPosition(x, y++);
+                Console.Write("  /\\  ");
+                Console.SetCursorPosition(x, y++);
+                Console.Write("[-||-]");
+                Console.SetCursorPosition(x, y++);
+                Console.Write("  ||  ");
+                Console.SetCursorPosition(x, y++);
+                Console.Write("[-\\/-]");
+            }
+        }
+
+        private void ClearOldCarPosition(int x, int y)
+        {
+            string carClearRow = new string(' ', Config.CarWidth);
+
+            lock (Game.consoleLocker)
+            {
+                for (int i = 0; i < this.CarHeight; i++)
+                {
+                    Console.SetCursorPosition(x, y + i);
+                    Console.Write(carClearRow);
+                }
+            }
+        }
+    }
+}
